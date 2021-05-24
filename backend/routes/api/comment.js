08624ef92be016727f742bc73dbf5f1068edc7a5
Jack <fighterbee87@gmail.com>
2021-05-24 15:51:26 -0700

    add comment works

--- backend/routes/api/comment.js
+++ backend/routes/api/comment.js
@@ -15,13 +15,13 @@ router.get('/:photoId',asyncHandler(async(req,res)=>{
 }))
 
 router.post('/:photoId',asyncHandler(async(req,res)=>{
-    const {photoId}=req.params.photoId
+    const photoId=req.params.photoId
     const {newComment,userId,id}=req.body;
     const content=newComment;
+    console.log()
+    const comment = await Comment.create({content,userId,photoId})
     
-    await Comment.create({content,userId,photoId})
-    
-    
+    return res.json(comment)
     
 }))
 
