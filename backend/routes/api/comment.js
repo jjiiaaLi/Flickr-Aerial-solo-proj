9818b7192a75d55cee6738df0289f3c24d884679
Jack <fighterbee87@gmail.com>
2021-05-24 15:12:21 -0700

    comment section with add comment error

--- backend/routes/api/comment.js
+++ backend/routes/api/comment.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const router = express.Router();
 const {Comment} = require('../../db/models');
 
@@ -13,10 +14,16 @@ router.get('/:photoId',asyncHandler(async(req,res)=>{
     res.json(comments)
 }))
 
-// router.post('/:photoId',asyncHandler(async(req,res)=>{
+router.post('/:photoId',asyncHandler(async(req,res)=>{
+    const {photoId}=req.params.photoId
+    const {newComment,userId,id}=req.body;
+    const content=newComment;
     
-
-// }))
+    await Comment.create({content,userId,photoId})
+    
+    
+    
+}))
 
 
 module.exports = router;
